56f797c187310ea064e68c10ffa7abf9f4b51481
Unknown <fiorellapsc@gmail.com>
2017-10-28 12:43:01 -0300

    post con referencias de proyecto y nota

--- src/routes/proyectos.js
+++ src/routes/proyectos.js
@@ -53,7 +53,7 @@ router.get('/usuario/:_id', (req, res) => {
 
 
 
-router.post('/', (req, res) => {
+router.post('/:_id', (req, res) => {
 
   var proyect = new Proyecto(req.body
 
@@ -71,7 +71,33 @@ router.post('/', (req, res) => {
 
   //una vez creada se guarda en la base de datos
   proyect.save().then(function() {
-    res.json(proyect);
+
+      Usuario.findById(
+          req.params._id
+        ).then(function(usuario) {
+          //actualiza la referencia al usuario
+          usuario.proyectos.push(proyect._id);
+          usuario.save().then(function(){
+              res.json(proyect);
+          }, function(err){
+
+            //Si no puede actualizar el usuario se debe borrar el proyecto ya guardado
+            /*
+            Proyecto.findByIdAndRemove(
+              req.params._id
+            ).then(function() {
+              res.json({
+                message: 'No se pudo crear el proyecto'
+              });
+            }, function(err) {
+              res.send(err);
+            });
+            */
+            res.send(err);
+          });
+        }, function(err) {
+          res.send(err);
+        });
 
   }, function(err) {
     res.send(err);
