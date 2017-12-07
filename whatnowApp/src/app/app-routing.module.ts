4c8f79c9cfbdbae84bef2eb3f27363b59a7e3188
Unknown <fiorellapsc@gmail.com>
2017-12-06 23:18:18 -0300

    InicioCrearEventoComponent

--- whatnowApp/src/app/app-routing.module.ts
+++ whatnowApp/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { BienvenidaComponent } from './components/bienvenida/bienvenida.component';
+import { CreareventoComponent } from './components/crearevento/crearevento.component';
 import { PageNotFoundComponent } from './not-found.component';
 
 
 const routes: Routes = [
 
   { path: 'bienvenida', component: BienvenidaComponent },
+  { path: 'crearEvento', component: CreareventoComponent },
   //{ path: 'wizardAsignarPersonal', component: WizardAsignarPersonalComponent },
   //{ path: 'wizardTipoPiezaInspeccionada', component: WizardTipoPiezaInspeccionadaComponent },
   //{ path: 'wizardTrabajosSupervisadosEmpleado', component: WizardTrabajosSupervisadosEmpleadoComponent },
--- whatnowApp/src/app/app.module.ts
+++ whatnowApp/src/app/app.module.ts
@@ -4,6 +4,40 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+//Material
+
+import {
+  MatAutocompleteModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSortModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatStepperModule
+} from '@angular/material';
+
+
 //services
 import { AsignarPersonalService } from './services/asignarPersonal.service';
 
@@ -16,6 +50,44 @@ import { PageNotFoundComponent } from './not-found.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { SidebarComponent } from './components/shared/sidebar/sidebar.component';
 import { SidebarModule } from 'ng-sidebar';
+import { CreareventoComponent } from './components/crearevento/crearevento.component';
+
+@NgModule({
+  exports: [
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatStepperModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule
+  ],
+  declarations: []
+})
+export class MaterialModule { }
+
+
 
 @NgModule({
   declarations: [
@@ -23,7 +95,8 @@ import { SidebarModule } from 'ng-sidebar';
     PageNotFoundComponent,
     BienvenidaComponent,
     NavbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    CreareventoComponent
   ],
   imports: [
     BrowserModule,
@@ -32,7 +105,8 @@ import { SidebarModule } from 'ng-sidebar';
     FormsModule,
     ReactiveFormsModule,
     HttpModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
   ],
   providers: [AsignarPersonalService],
   bootstrap: [AppComponent]
